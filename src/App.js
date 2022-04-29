6702835e7c0a6cc52e60d28ade69767d24f9d8e9
nanaho-mu <nanaho.mu0823@gmail.com>
2022-04-29 21:56:13 +0900

    create .env

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const CLIENT_ID=process.env.REACT_APP_CLIENT_ID
+  const REDIRECT_URI=process.env.REACT_APP_REDIRECT_URI
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
 return (
   <>
   <h1>hello</h1>
