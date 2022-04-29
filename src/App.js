e15afb0eaaae9e96a412f604d434d43c5808084a
nanaho-mu <nanaho.mu0823@gmail.com>
2022-04-29 22:01:22 +0900

    install spotifywebapi token localStorageからデータを取得

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ function App() {
   const REDIRECT_URI=process.env.REACT_APP_REDIRECT_URI
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
+
+  const [token, setToken]=useState("")
+
+  useEffect(()=>{
+    const hash=window.location.hash
+    // localStorageからデータを取得する,取得したいデータのkeyを指定して取り出せる
+    let token=window.localStorage.getItem("token")
+
+    if (!token&&hash){
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      console.log(token)
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+    setToken(token)
+  },[])
+
 return (
   <>
   <h1>hello</h1>
